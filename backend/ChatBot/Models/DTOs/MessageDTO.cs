fb957ae9277a16be2c34a65b4b1fcca8a79159e2
Rasim, Mertan M <m.rasim@student.fontys.nl>
2022-10-03 01:52:14 +0200

    Co-authored: Balsa Stevovic, Lachezar Mitov Small contributions: Tihomir Kandev, Radoslav Radev

---
+++ backend/ChatBot/Models/DTOs/MessageDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChatBot.Models.DTOs;
+
+public record MessageDTO
+{
+    public Guid AuthorID { get; set; }
+    
+    public string Content { get; set; }
+    
+    public string Nickname { get; set; }
+    
+    //public DateTime Timestamp { get; set; }
+
+    public MessageAction Action { get; set; }
+}
+public enum MessageAction
+{
+    JOIN = 0,
+    SEND = 1,
+    LEAVE = 2,
+}
---
+++ backend/ChatBot/Program.cs
@@ -0,0 +1,54 @@
+using ChatBot;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using ChatBot.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<TicketService>();
+builder.Services.AddRazorPages();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(240);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseWebSockets(new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromSeconds(60),
+});
+
+//app.UseMiddleware<WebsocketHandlerMiddleware>();
+app.UseMiddleware<ConversationMiddleware>();
+
+app.UseStaticFiles();
+
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoint => { endpoint.MapControllers(); });
+
+app.MapRazorPages();
+
+app.Run();
