0d02d26a4f6e9368235134792b66fba6e11f2a74
Lachezar <l.mitov@student.fontys.nl>
2022-10-14 23:51:26 +0200

    Implemnted Login UI

--- frontend/src/app.properties.ts
+++ frontend/src/app.properties.ts
@@ -4,6 +4,7 @@ export const techSupportNickName = 'Bas World Representative';
 export const botNickName = 'bot';
 export const BaseUrl = 'http://localhost:5000';
 export const TicketApi = '/ticket';
+export const LoginApi = '/auth/login';
 export const ChatBotOpennigMessage = 'Hello, welcome to the Bas World Chat support, my name is Botty. In the future i will be able to assist you further, but for now i will connect you to a support agent';
 export const ChatBot2OpenningMessage = 'To start i need to ask you a couple of questions!';
 export const webSocketServerAddress = 'ws://localhost:5000/ws';
---
+++ frontend/src/components/Login/userApi.ts
@@ -0,0 +1,10 @@
+﻿import axios from "axios";
+import {BaseUrl, LoginApi} from "../../app.properties";
+
+const login = async (request) => {
+    return await axios.post(BaseUrl + LoginApi, request);
+}
+
+export default {
+    login
+}
