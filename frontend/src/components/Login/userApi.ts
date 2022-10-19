b2a2dd034481ae2075000778212488f2e92c016c
Mitov,Lachezar L.G <l.mitov@student.fontys.nl>
2022-10-19 14:54:25 +0000

    Revert "Implemented Login and Register UI"
    This reverts commit fd44d4acdeabfd783c8be2e9cfcc6775eee54dcf

--- frontend/src/components/Login/userApi.ts
+++ frontend/src/components/Login/userApi.ts
@@ -2,19 +2,9 @@
 import {BaseUrl, LoginApi} from "../../app.properties";
 
 const login = async (request) => {
-        return await axios.post(BaseUrl + LoginApi, request).then((response) => {
-            if (response.data) {
-                localStorage.setItem('user', JSON.stringify(response.data));
-            }
-            return response.data;
-        })
-};
-
-const getCurrentUser = () => {
-    return JSON.parse(localStorage.getItem('user')) || null;
-};
+    return await axios.post(BaseUrl + LoginApi, request);
+}
 
 export default {
-    login,
-    getCurrentUser
+    login
 }
