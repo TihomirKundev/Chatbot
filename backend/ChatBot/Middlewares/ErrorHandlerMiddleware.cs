5014e4411f622c2123b75243516b723ddae71755
Rasim, Mertan M <m.rasim@student.fontys.nl>
2022-11-01 05:14:18 +0100

    Fixed Error handler

--- backend/ChatBot/Middlewares/ErrorHandlerMiddleware.cs
+++ backend/ChatBot/Middlewares/ErrorHandlerMiddleware.cs
@@ -14,12 +14,10 @@ namespace ChatBot.Middlewares;
 public class ErrorHandlerMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger _logger;
 
-    public ErrorHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+    public ErrorHandlerMiddleware(RequestDelegate next)
     {
         _next = next;
-        _logger = loggerFactory.CreateLogger<ErrorHandlerMiddleware>(); 
     }
 
     public async Task Invoke(HttpContext context)
@@ -28,33 +26,30 @@ public class ErrorHandlerMiddleware
         {
             await _next(context);
         }
-        catch (System.Exception ex)
+        catch (Exception ex)
         {
             var response = context.Response;
             response.ContentType = "application/json";
-            
-            switch (ex)
+
+            response.StatusCode = ex switch
             {
-                
-                case AggregateException:
-                case KeyNotFoundException:
-                case TokenNotFoundException:
-                case UserNotFoundException e:
-                    response.StatusCode = (int)HttpStatusCode.NotFound;
-                    break;
-                case FormatException:
-                case ArgumentNullException:
-                case DuplicateEmailException:
-                case InvalidCredentialsException e:
-                    response.StatusCode = (int)HttpStatusCode.BadRequest;
-                    break;
-                default:
-                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    break;
-            }
-            
-            // var result = ex.Message;
-            // await response.WriteAsync(result);
+                AggregateException or 
+                KeyNotFoundException or 
+                TokenNotFoundException or 
+                UserNotFoundException 
+                    => (int)HttpStatusCode.NotFound,
+                    
+                FormatException or
+                ArgumentNullException or 
+                DuplicateEmailException or 
+                InvalidCredentialsException 
+                    => (int)HttpStatusCode.BadRequest,
+                    
+                _ => (int)HttpStatusCode.InternalServerError,
+            };
+
+            var result = ex.Message;
+            await response.WriteAsync(result);
         }
     }
 
