eba8616818dc7b633d71c2b02de286c170c34a73
Lachezar <l.mitov@student.fontys.nl>
2022-11-28 13:20:37 +0100

    Secured URLs to prevent unauthorized access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,22 @@ import ChatBot from "./components/Customer/ChatBot";
 import LoginPage from "./components/Login/LoginPage";
 import RegisterPage from "./components/Register/RegisterPage";
 import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
+import userApi from "./components/Login/userApi"; 
 
 function App() {
+    
+    const user = userApi.getCurrentUser();
+    
     return (
         <Router>
-
             <div className="App">
                 <Switch>
-                    <Route path="/customer"  element={<ChatBot/>}/>
-                    <Route path="/admin"  element={<TechSupportPage/>}/>
+                    <Route path="/customer"  element={user && user.role === "CUSTOMER" ? <ChatBot/> : <LoginPage/> }/>
+                    <Route path="/admin"  element={user && user.role === "ADMIN" ? <TechSupportPage/> : <LoginPage/>}/>
                     <Route path="/" element={<LoginPage/>}/>
                     <Route path="/register" element={<RegisterPage/>}/>
                 </Switch>
             </div>
-
-
         </Router>
     );
 }
