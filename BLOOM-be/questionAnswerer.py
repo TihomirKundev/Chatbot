5053e08a959c86d3f7b1f102218a1ebcb9733885
Lachezar <l.mitov@student.fontys.nl>
2022-12-10 13:56:57 +0100

    Modified AI prompt for FAQ questions to contain links when answering some questions for better UX.

--- BLOOM-be/questionAnswerer.py
+++ BLOOM-be/questionAnswerer.py
@@ -22,10 +22,10 @@ def answerFAQQuestion(UserQuestion):
     sample = model.generate(**input_ids, max_length=1255, top_k=0, temperature=1.7)
     result: str = tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"])
     slicedRes = result[1205 + len(UserQuestion):len(result)]  # get only the answer
-    if("A25:" in slicedRes):
-        slicedRes = slicedRes[slicedRes.find("A25")+4:] # remove the A25: from the beginning of the answer
-    if("Q26" in slicedRes):
-        slicedRes = slicedRes[0:slicedRes.find("Q26")]
+    if("A27:" in slicedRes):
+        slicedRes = slicedRes[slicedRes.find("A27")+4:] # remove the A25: from the beginning of the answer
+    if("Q27" in slicedRes):
+        slicedRes = slicedRes[0:slicedRes.find("Q27")]
     if("." in slicedRes):
         slicedRes = slicedRes[0:slicedRes.find(". ")]
     return slicedRes
