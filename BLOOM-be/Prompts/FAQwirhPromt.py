5e6c99a03d243465aa162dff88c81b7b94a12fee
Lachezar <l.mitov@student.fontys.nl>
2022-12-11 17:58:56 +0100

    Modified AI FAQ Questions prompt for better performance of the AI model.

--- BLOOM-be/Prompts/FAQwirhPromt.py
+++ BLOOM-be/Prompts/FAQwirhPromt.py
@@ -1,59 +1,60 @@
 ﻿prompt = {
-"What can I do when I have issues with my account?": "Please contact our BAS World service desk. Our experts of the service desk can help you with all your questions regarding account issues.",
-"Will you send me an update if new vehicles become available that match my Saved Search criteria?": "Yes, we will! We send BAS World Saved Search email update and show you newly available vehicles.",
-"Why should I mark a vehicle as a favourite?": "You can see all your favourite vehicles quickly and easily in your BAS World dashboard – and you don’t have to search for these again. You will also get updates about any price reductions and see when a vehicle is sold.",
-"What will happen after I placed my order?": "After the order is placed and the payment is fullfilled the required documents will be sent by the seller. When the required documents are sent, the vehicle can be picked up, or will be delivered to your location.",
-"Can I buy a vehicle located in another country?": "Sure, why not? You need to import a vehicle to your country, but that shouldn’t be too difficult. However, please keep in mind that there are different rules regarding the import of a vehicle, and that you have verified any applicable customs fees and taxes. We advise you to seek advice from your local registration authority and contry's customs office at all times.",
+    "What can I do when I have issues with my account?": "Please contact our BAS World service desk. Our experts of the service desk can help you with all your questions regarding account issues.",
+    "Will you send me an update if new vehicles become available that match my Saved Search criteria?": "Yes, we will! We send BAS World Saved Search email update and show you newly available vehicles.",
+    "Why should I mark a vehicle as a favourite?": "You can see all your favourite vehicles quickly and easily in your BAS World dashboard – and you don’t have to search for these again. You will also get updates about any price reductions and see when a vehicle is sold.",
+    "What will happen after I placed my order?": "After the order is placed and the payment is fullfilled the required documents will be sent by the seller. When the required documents are sent, the vehicle can be picked up, or will be delivered to your location.",
+    "Can I buy a vehicle located in another country?": "Sure, why not? You need to import a vehicle to your country, but that shouldn’t be too difficult. However, please keep in mind that there are different rules regarding the import of a vehicle, and that you have verified any applicable customs fees and taxes. We advise you to seek advice from your local registration authority and contry's customs office at all times.",
+    "Is it safe to buy a vehicle without seeing the vehicle in real life?": "Many people buy vehicles without a physical inspection these days. Photos, videos, specifications and detailed information about the condition can give you enough information to make the decision to buy a vehicle – which also saves you time and money.",
 }
 
-faqPromt = '''
-Q1:What can I do when I have issues with my account?
-A1:Please contact our BAS World service desk. Our experts of the service desk can help you with all your questions regarding account issues.
-Q2:Will you send me an update if new vehicles become available that match my Saved Search criteria?
-A2Yes, we will! We send BAS World Saved Search email update and show you newly available vehicles.
-Q3:Why should I mark a vehicle as a favourite?
-A3:You can see all your favourite vehicles quickly and easily in your BAS World dashboard – and you don’t have to search for these again. You will also get updates about any price reductions and see when a vehicle is sold.
-Q4:What will happen after I placed my order?
-A4:After the order is placed and the payment is fullfilled the required documents will be sent by the seller. When the required documents are sent, the vehicle can be picked up, or will be delivered to your location.
-Q5:Can I buy a vehicle located in another country?
-A5:Sure, why not? You need to import a vehicle to your country, but that shouldn’t be too difficult. However, please keep in mind that there are different rules regarding the import of a vehicle, and that you have verified any applicable customs fees and taxes. We advise you to seek advice from your local registration authority and contry's customs office at all times.
-Q6:Is it safe to buy a vehicle without seeing the vehicle in real life?
-A6:Many people buy vehicles without a physical inspection these days. Photos, videos, specifications and detailed information about the condition can give you enough information to make the decision to buy a vehicle – which also saves you time and money.
-Q7:What is “Saved Search”?
-A7:When you do a detailed search on BAS World, you can save this search so you can perform the very same detailed search at a later point in time with just a single click. Our system can also send you updates to inform you about newly listed vehicles that match the criteria of your Saved Searches – to make sure you’re always the first to know about newly listed vehicles that may be just what you are looking for.
-Q8:I want to buy a specific vehicle, how do I get in touch with the seller?
-A8:Just fill in your prefered specifications, choose the desired vehicle and start a chat with the seller on www.basworld.com or via our application.
-Q9:How many vehicles can I add to my watch list?
-A9:As many as you like.
-Q10:Is my information safe on BAS World?
-A10:It is! We keep all data safe and secure in line with laws and regulations in the EU.
-Q11:How do I change my delivery address after I have ordered a vehicle?
-A11:Contact your seller via BAS World chat and tell him or her the new delivery address.
-Q12:Where can I find the vessel schedule?
-A12:In your transport confirmation you can find all details on the scheme.
-Q13:Can I change the delivery address after the transport confirmation?
-A13:No this is only possible before the transport confirmation.
-Q14:Can I trace my vehicle during transport if BAS World has arranged the delivery?
-A14:We will give updates during each step in the transport process when you choose for BAS World's Delivery Service.
-Q15:Do I have to pay taxes or fees when importing a vehicle?
-A15:Probably you will have to. The tax and fee structure differs per country as each country has different regulations. Contact your country’s relevant institution to enquiry about possible taxes and fees.
-Q16:When can I expect the Bill of Lading after my vehicle is shipped?
-A16:As soon as the vehicle is on its way, the Bill of Lading (BL) will be drafted. At that moment you will be informed and provided with the BL.
-Q17:How do I get the vehicle to my business address?
-A17:This depends on the location of the vehicle. You can choose to pick up the vehicle directly from the seller, or have the vehicle transported or shipped to your preferred port. By using the Safe Deal service, you have the possibility to let us arrange the delivery of your vehicle. You can find more information about Safe Deal here: Safe deal.
-Q18:How do I get my vehicle out of the port?
-A18:This differs in every port. For the correct information please contact the port of arrival or the company who arranged shipping.
-Q19:How am I insured on transit plates?
-A19:Transit plates have third party liability insurance, which means the vehicle is insured for damages caused to others. Please always make sure you check if the coverage is sufficient for you.
-Q20:How am I insured for transport?
-A20:By choosing Safe Deal you can make use of our delivery service, by using the delivery service provided by us you are insured under the CMR conditions. If you don't make use of our delivery service you'll have to discuss with your local insurance agent what the ideal insurance option would be.
-Q21:Do you offer a delivery guarantee?
-A21:If you're using Safe Deal, a guarantee will be offered that the vehicle in the pictures will actually be delivered.
-Q22:Do I need to buy a vignette or pay toll when I want to pick up my vehicle over the road?
-A22:This is different in every country, you have to check this at the border of every country you enter.
-Q23:Can I reach the company that transports my vehicles to me?
-A23:If you have chosen for the transport option offered by BAS World you can always contact our delivery desk to get the desired information.
-Q24:What do I need to fill in on a CMR?
-A24:Please see the following example for a complete signed: CMR proof of delivery.
-Q25:
-'''
+# faqPromt = '''
+# Q1:What can I do when I have issues with my account?
+# A1:Please contact our BAS World service desk. Our experts of the service desk can help you with all your questions regarding account issues.
+# Q2:Will you send me an update if new vehicles become available that match my Saved Search criteria?
+# A2Yes, we will! We send BAS World Saved Search email update and show you newly available vehicles.
+# Q3:Why should I mark a vehicle as a favourite?
+# A3:You can see all your favourite vehicles quickly and easily in your BAS World dashboard – and you don’t have to search for these again. You will also get updates about any price reductions and see when a vehicle is sold.
+# Q4:What will happen after I placed my order?
+# A4:After the order is placed and the payment is fullfilled the required documents will be sent by the seller. When the required documents are sent, the vehicle can be picked up, or will be delivered to your location.
+# Q5:Can I buy a vehicle located in another country?
+# A5:Sure, why not? You need to import a vehicle to your country, but that shouldn’t be too difficult. However, please keep in mind that there are different rules regarding the import of a vehicle, and that you have verified any applicable customs fees and taxes. We advise you to seek advice from your local registration authority and contry's customs office at all times.
+# Q6:Is it safe to buy a vehicle without seeing the vehicle in real life?
+# A6:Many people buy vehicles without a physical inspection these days. Photos, videos, specifications and detailed information about the condition can give you enough information to make the decision to buy a vehicle – which also saves you time and money.
+# Q7:What is “Saved Search”?
+# A7:When you do a detailed search on BAS World, you can save this search so you can perform the very same detailed search at a later point in time with just a single click. Our system can also send you updates to inform you about newly listed vehicles that match the criteria of your Saved Searches – to make sure you’re always the first to know about newly listed vehicles that may be just what you are looking for.
+# Q8:I want to buy a specific vehicle, how do I get in touch with the seller?
+# A8:Just fill in your prefered specifications, choose the desired vehicle and start a chat with the seller on www.basworld.com or via our application.
+# Q9:How many vehicles can I add to my watch list?
+# A9:As many as you like.
+# Q10:Is my information safe on BAS World?
+# A10:It is! We keep all data safe and secure in line with laws and regulations in the EU.
+# Q11:How do I change my delivery address after I have ordered a vehicle?
+# A11:Contact your seller via BAS World chat and tell him or her the new delivery address.
+# Q12:Where can I find the vessel schedule?
+# A12:In your transport confirmation you can find all details on the scheme.
+# Q13:Can I change the delivery address after the transport confirmation?
+# A13:No this is only possible before the transport confirmation.
+# Q14:Can I trace my vehicle during transport if BAS World has arranged the delivery?
+# A14:We will give updates during each step in the transport process when you choose for BAS World's Delivery Service.
+# Q15:Do I have to pay taxes or fees when importing a vehicle?
+# A15:Probably you will have to. The tax and fee structure differs per country as each country has different regulations. Contact your country’s relevant institution to enquiry about possible taxes and fees.
+# Q16:When can I expect the Bill of Lading after my vehicle is shipped?
+# A16:As soon as the vehicle is on its way, the Bill of Lading (BL) will be drafted. At that moment you will be informed and provided with the BL.
+# Q17:How do I get the vehicle to my business address?
+# A17:This depends on the location of the vehicle. You can choose to pick up the vehicle directly from the seller, or have the vehicle transported or shipped to your preferred port. By using the Safe Deal service, you have the possibility to let us arrange the delivery of your vehicle. You can find more information about Safe Deal here: Safe deal.
+# Q18:How do I get my vehicle out of the port?
+# A18:This differs in every port. For the correct information please contact the port of arrival or the company who arranged shipping.
+# Q19:How am I insured on transit plates?
+# A19:Transit plates have third party liability insurance, which means the vehicle is insured for damages caused to others. Please always make sure you check if the coverage is sufficient for you.
+# Q20:How am I insured for transport?
+# A20:By choosing Safe Deal you can make use of our delivery service, by using the delivery service provided by us you are insured under the CMR conditions. If you don't make use of our delivery service you'll have to discuss with your local insurance agent what the ideal insurance option would be.
+# Q21:Do you offer a delivery guarantee?
+# A21:If you're using Safe Deal, a guarantee will be offered that the vehicle in the pictures will actually be delivered.
+# Q22:Do I need to buy a vignette or pay toll when I want to pick up my vehicle over the road?
+# A22:This is different in every country, you have to check this at the border of every country you enter.
+# Q23:Can I reach the company that transports my vehicles to me?
+# A23:If you have chosen for the transport option offered by BAS World you can always contact our delivery desk to get the desired information.
+# Q24:What do I need to fill in on a CMR?
+# A24:Please see the following example for a complete signed: CMR proof of delivery.
+# Q25:
+# '''
--- BLOOM-be/questionAnswerer.py
+++ BLOOM-be/questionAnswerer.py
@@ -6,39 +6,29 @@ from Prompts.OrderWithPrompt import OrderWithPrompt
 
 import Cache
 
-
 import nltk
 import sklearn
 
+
 def FAQanswer(question):
-  prompt_dict = FAQwirhPromt.prompt
-
-  # Use NLTK's word_tokenize method to split the question into individual words
-  tokenized_question = nltk.word_tokenize(question)
-
-  # Create an empty list to store the tokenized questions from the prompt dictionary
-  tokenized_prompts = []
-
-  # Iterate through the prompt dictionary and tokenize the questions
-  for key, value in prompt_dict.items():
-    tokenized_prompts.append(nltk.word_tokenize(key))
-
-  # Use scikit-learn's CountVectorizer to create a bag-of-words representation of the tokenized text
-  vectorizer = sklearn.feature_extraction.text.CountVectorizer()
-  question_vectors = vectorizer.fit_transform(tokenized_question)
-  prompt_vectors = vectorizer.transform(tokenized_prompts)
-
-  # Use scikit-learn's cosine similarity method to find the similarity between the question and the prompt dictionary
-  similarity = sklearn.metrics.pairwise.cosine_similarity(question_vectors, prompt_vectors)
-
-  # Find the index of the most similar question in the prompt dictionary to the question
-  most_similar_question_index = similarity.argmax()
-
-  # Return the answer from the prompt dictionary that corresponds to the most similar question
-  return prompt_dict[tokenized_prompts[most_similar_question_index]]
-
-
-
+    prompt_dict = FAQwirhPromt.prompt
+    # Use NLTK's word_tokenize method to split the question into individual words
+    tokenized_question = nltk.word_tokenize(question)
+    # Create an empty list to store the tokenized questions from the prompt dictionary
+    tokenized_prompts = []
+    # Iterate through the prompt dictionary and tokenize the questions
+    for key, value in prompt_dict.items():
+        tokenized_prompts.append(nltk.word_tokenize(key))
+    # Use scikit-learn's CountVectorizer to create a bag-of-words representation of the tokenized text
+    vectorizer = sklearn.feature_extraction.text.CountVectorizer()
+    question_vectors = vectorizer.fit_transform(tokenized_question)
+    prompt_vectors = vectorizer.transform(tokenized_prompts)
+    # Use scikit-learn's cosine similarity method to find the similarity between the question and the prompt dictionary
+    similarity = sklearn.metrics.pairwise.cosine_similarity(question_vectors, prompt_vectors)
+    # Find the index of the most similar question in the prompt dictionary to the question
+    most_similar_question_index = similarity.argmax()
+    # Return the answer from the prompt dictionary that corresponds to the most similar question
+    return prompt_dict[tokenized_prompts[most_similar_question_index]]
 
 
 # torch.set_default_tensor_type(torch.cuda.FloatTensor)
@@ -49,37 +39,39 @@ def FAQanswer(question):
 
 model, tokenizer = Cache.getModels();
 
+
 # set_seed(42)
 
 def answerFAQQuestion(UserQuestion):
     prompt = FAQwirhPromt.faqPromt + UserQuestion
-    input_ids = tokenizer(prompt, return_tensors="pt") # .to(0)
+    input_ids = tokenizer(prompt, return_tensors="pt")  # .to(0)
     sample = model.generate(**input_ids, max_length=1255, top_k=0, temperature=1.7)
     result: str = tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"])
     slicedRes = result[1205 + len(UserQuestion):len(result)]  # get only the answer
-    if("A25:" in slicedRes):
-        slicedRes = slicedRes[slicedRes.find("A25")+4:] # remove the A25: from the beginning of the answer
-    if("Q26" in slicedRes):
+    if ("A25:" in slicedRes):
+        slicedRes = slicedRes[slicedRes.find("A25") + 4:]  # remove the A25: from the beginning of the answer
+    if ("Q26" in slicedRes):
         slicedRes = slicedRes[0:slicedRes.find("Q26")]
-    if("." in slicedRes):
+    if ("." in slicedRes):
         slicedRes = slicedRes[0:slicedRes.find(". ")]
     return slicedRes
 
+
 def answerOrderQuestion(UserQuestion: orderQuestionDTO):
     prompt = OrderWithPrompt(UserQuestion)
-    input_ids = tokenizer(prompt, return_tensors="pt") # .to(0)
+    input_ids = tokenizer(prompt, return_tensors="pt")  # .to(0)
     sample = model.generate(**input_ids, max_length=170, top_k=0, temperature=1.7)
     result: str = tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"])
-    slicedRes = result[result.find("Answer:")+7:len(result)]  # get only the answer
-    if("  \nA:" in slicedRes):
-        slicedRes = slicedRes[slicedRes.find("  \nA:")+6:] # remove the A: from the beginning of the answer    
-        if("\nB:" in slicedRes):
+    slicedRes = result[result.find("Answer:") + 7:len(result)]  # get only the answer
+    if ("  \nA:" in slicedRes):
+        slicedRes = slicedRes[slicedRes.find("  \nA:") + 6:]  # remove the A: from the beginning of the answer
+        if ("\nB:" in slicedRes):
             slicedRes = slicedRes[0:slicedRes.find("\nB:")]
-        if("\nA:" in slicedRes):
+        if ("\nA:" in slicedRes):
             slicedRes = slicedRes[0:slicedRes.find("\nA:")]
     else:
-        if("A:" in slicedRes):
-            slicedRes = slicedRes[0:slicedRes.find("A:")] # remove the A25: from the beginning of the answer
-        if("." in slicedRes):
+        if ("A:" in slicedRes):
+            slicedRes = slicedRes[0:slicedRes.find("A:")]  # remove the A25: from the beginning of the answer
+        if ("." in slicedRes):
             slicedRes = slicedRes[0:slicedRes.find(". ")]
     return slicedRes
