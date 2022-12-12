2adc02e4fff8857ee21a4a44ede29ce3789f1c16
Radoslav Radev <r.radev@student.fontys.nl>
2022-12-12 15:39:46 +0100

    added link detection and support in the front end

--- frontend/src/components/Customer/ChatMessage.tsx
+++ frontend/src/components/Customer/ChatMessage.tsx
@@ -1,9 +1,9 @@
 ﻿import './style/style.css';
-
+import Linkify from 'react-linkify';
 const ChatMessage = ({message, fromSelf} : {message: string, fromSelf: boolean}) => 
     <div className={`chat-msg ${fromSelf ? 'self' : 'user'}`}>
         <span className="msg-avatar"></span>
-        <div className="cm-msg-text">{message}</div>
+        <div className="cm-msg-text"><Linkify>{message}</Linkify></div>
     </div>;
 
 export default ChatMessage;
